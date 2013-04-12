3d7dc44a41def2761850a1b8302a7e04df7f63d8
元兒～ <yuan817@moztw.org>
2013-04-12 20:57:51 +0800

    First commit

---
+++ gen/org/twbbs/yuan817/HTMLHello/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.twbbs.yuan817.HTMLHello;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/org/twbbs/yuan817/HTMLHello/MainActivity.java
@@ -0,0 +1,24 @@
+package org.twbbs.yuan817.HTMLHello;
+
+import org.apache.cordova.*;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl(Config.getStartUrl());
+		//setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
